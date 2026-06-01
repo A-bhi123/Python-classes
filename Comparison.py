845d25bb3c072200017ce8285b9daee53174a3af
A-bhi123 <abhishekkumarbind616@gmail.com>
2026-06-01 12:21:23 +0530

    Comparison Operator

---
+++ Comparison.py
@@ -0,0 +1,20 @@
+a = int(input("Enter the first number: ",))
+b = int(input("Enter the second number: ",))
+
+# Equal to
+print("A is equal to B:",a == b)   # False
+
+# Not equal to
+print("A Not equal to B:",a != b)   # True
+
+# Greater than
+print("A is Greater than B:",a > b)    # True
+
+# Less than
+print("A Less than B:",a < b)    # False
+
+# Greater than or equal to
+print("A Greater than or equal to B:",a >= b)   # True
+
+# Less than or equal to
+print("A Less than or equal to B:",a <= b)   # False
