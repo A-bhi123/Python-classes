ad01530a4ece26dd71083db6c645194faa448781
A-bhi123 <abhishekkumarbind616@gmail.com>
2026-06-01 12:26:37 +0530

    Logical Operator

---
+++ Logical.py
@@ -0,0 +1,12 @@
+a = True
+b = False
+
+# AND operator
+print("a and b:", a and b)   # False
+
+# OR operator
+print("a or b:", a or b)     # True
+
+# NOT operator
+print("not a:", not a)       # False
+print("not b:", not b)       # True
